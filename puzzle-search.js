4d904b5a0d3678e5715fef91b46804cab23a7c68
2KOH <ueda@uedayasuhironoMacBook-Air.local>
2022-05-13 14:33:13 +0900

    a

---
+++ puzzle-search.js
@@ -0,0 +1,23 @@
+const search = function*(string) {
+	const words = string.toLowerCase().normalize('NFKC').split(/\s/)
+	for (const [id, names] of puzzleNames) {
+		if (names.some(
+			name => words.every(
+				word => name.includes(word)
+			)
+		)) {
+			yield id
+		}
+	}
+}
+
+const searchText = document.querySelector('#search-text')
+const liList = document.querySelectorAll('#search-text ~ ul > li')
+
+searchText.addEventListerner('input', () => {
+	const regExp = new RegExp(`^${ [...search(searchText.value)].join('|') }$`)
+	for (const li of liList) {
+		const id = li.querySelector('a').herf.match(/^puzzle\/(.*).md$/)[1]
+		li.style.display = regExp.test(id) ? '' : 'none'
+	}
+})
